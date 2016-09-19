b6652e579f769eeabeb6ae64db3903043da00ac9
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-09-19 22:32:15 +0000

    added term handler

--- inetd.c
+++ inetd.c
@@ -8,6 +8,10 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <signal.h>
+#include <malloc.h>
+volatile sig_atomic_t hup_received = 0;
+volatile sig_atomic_t term_received = 0;
+
 
 static void readConfigServ(char *config_file, struct service_type **servs, size_t *num_servs){
 	if(servs == NULL || num_servs == NULL){
@@ -36,10 +40,6 @@ static void readConfigServ(char *config_file, struct service_type **servs, size_
 
 }
 
-static void stopAndCleanServ(void *servs){
-	//closes config and cleans up services
-	/
-}
 
 
 static void readAndRunServ(void){
@@ -58,45 +58,64 @@ static void readAndRunServ(void){
 	struct pollstate_type ps;
 	while(!pollStart(poll_servs,num_servs,&ps)){
 		if(!pollAccept(&servs[ps.current_index],poll_servs[ps.current_index].fd)){
-			if(errno = EINTR && hup_received)
+			if(errno = EINTR && (hup_received ||term_received))
 				return;
+			free(servs);
 			errnoExit("pollAccept");
 		}
 
 	}
-	if(errno = EINTR && hup_recieved)
+	if(errno = EINTR && (hup_received || term_received))
 		return;
+	free(servs);
 	errnoExit("pollStart");
 
 }
 
 
 
-volatile sig_atomic_t hup_received = 0;
 void hup_handler(int sig){
 
 	hup_received = 1;
 
 }
 
+void term_handler(int sig){
+	term_received = 1;
+}
+
 
 
 int main(int argc,char *argv[]){
 	struct sigaction hup_action;
-	hup_action.sa_flags = SA_RESTART;
+	hup_action.sa_flags = 0;
 	sigemptyset(&hup_action.sa_mask);
 	hup_action.sa_handler = hup_handler;
-	if(sigaction(SIGHUP,&hup_action,NULL)  == -1){
+	if(sigaction(SIGHUP,&hup_action,NULL)  == -1)
+		errnoExit("sigaction");
+	struct sigaction term_action;
+	term_action.sa_flags = 0;
+	sigemptyset(&term_action.sa_mask);
+	term_action.sa_handler = term_handler;
+	if(sigaction(SIGTERM,&term_action,NULL) == -1)
 		errnoExit("sigaction");
 
-	}
 
 	if(daemonize(0) == -1)
 		errExit("daemonize");
 
 	openlog(LOG_IDENF,0,LOG_USER);
-	readAndRun();
-	exit(EXIT_SUCCESS);
+	while(TRUE){
+		readAndRunServ();
+		if(term_received)
+			exit(EXIT_SUCCESS);
+		else if(!hup_received)
+			errExit("%s\n","unknown error");
+		else
+			hup_received = 0;
+				
+	}
+	exit(EXIT_FAILURE);
 
 }
 
